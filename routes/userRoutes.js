022d6a5a19e266f6934653f223cf046058c449db
prabhas-code <prabhasmadipadiga@gmail.com>
2025-12-31 11:48:56 +0530

    updated import issue in userRoute.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protect } from "../middlewares/authmiddleware.js";
+import { protect } from "../middlewares/authMiddleware.js";
 import {
   getProfile,
   updateProfile,
