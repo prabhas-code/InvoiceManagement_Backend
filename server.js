2f9c35b76df2200e01bae2acbeacbcb725262976
prabhas-code <prabhasmadipadiga@gmail.com>
2026-01-04 23:46:12 +0530

    updated cors

--- server.js
+++ server.js
@@ -12,7 +12,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5174/" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://127.0.0.1:5174",
+      "https://prabhas-user-management-frontend.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
