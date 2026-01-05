5ba4d634e7d3a0843019ce2b9ce340a5b8883335
prabhas-code <prabhasmadipadiga@gmail.com>
2026-01-05 08:44:08 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "http://127.0.0.1:5174",
       "https://prabhas-user-management-frontend.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
